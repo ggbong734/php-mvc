3b3a9f55e4387e93e3fda32633301bb7a8644f2c
unknown <ggbong734@gmail.com>
2022-11-27 20:15:13 -0800

    first commit up to config php

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // URL Root
+  define('URLROOT', 'http://localhost/traversymvc/public');
+
+  // Site name
+  define('SITENAME', 'TraversyMVC');
